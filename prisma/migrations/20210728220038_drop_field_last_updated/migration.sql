b69b7e26447e3277250d126007ee7ed16f414117
iamist <crivelvicente@gmail.com>
2021-07-29 06:05:27 +0800

    droping field last_updates

---
+++ prisma/migrations/20210728220038_drop_field_last_updated/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `last_updates` on the `Summary` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `Summary` DROP COLUMN `last_updates`;
